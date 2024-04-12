44a2599200a80dc21dd1616ca8b63ec0b422116e
Nikita Denisenok <nikitadenisenok@gmail.com>
2024-04-12 11:20:40 +0300

    updated init.sql

--- deploy/database/init.sql
+++ deploy/database/init.sql
@@ -4,24 +4,18 @@ create table features (
 );
 
 create table tags (
-    id bigbigserial primary key,
+    id bigserial primary key,
     name varchar(100) not null
 );
+
 
 
 create table banners (
     id bigserial primary key,
     content jsonb not null,
-    feature_id bigint,
     is_active boolean,
     created_at timestamp,
-    updated_at timestamp,
-    unique(id, feature_id),
-    constraint fk_feature 
-        foreign key (feature_id) 
-            references features(id)
-                on delete cascade
-                on update cascade
+    updated_at timestamp
 );
 
 create table banners_tags (
@@ -29,9 +23,17 @@ create table banners_tags (
         references banners(id)
             on delete cascade
             on update cascade,
+    feature_id bigint
+                references features(id)
+                on delete cascade
+                on update cascade,
     tag_id bigint 
         references tags(id)
             on delete cascade
             on update cascade,
+    unique(feature_id, tag_id),
     primary key (banner_id, tag_id)
 );
+
+create index ix_feature_id ON banners_tags using btree (feature_id);
+create index ix_tag_id ON banners_tags using btree (tag_id);
